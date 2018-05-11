17ad995007deaffed5c82a62744695376a604015
Ripudaman Singh <rsingh_bemba16@thapar.edu>
2018-05-12 02:11:30 +0530

    msg

--- Approapp/urls.py
+++ Approapp/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.conf.urls import url,include
 from django.contrib import admin
 from . import views
-from wkhtmltopdf.views import PDFTemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
