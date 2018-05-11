1211d94465cf1e6c4d6e45f93edfc00b2ee91412
rds0751 <rds0751@gmail.com>
2018-05-11 13:03:23 +0530

    msg2

--- Approapp/settings.py
+++ Approapp/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-	'hello_sign',
     'loginReg',
     'django.contrib.admin',
     'django.contrib.auth',
--- Approapp/urls.py
+++ Approapp/urls.py
@@ -24,7 +24,6 @@ urlpatterns = [
     url(r'^$', include('core.urls')),
     # View URLs
 	url(r'^fobi/', include('fobi.urls.view')),
-	url(r'^sign/', include('hello_sign.urls', namespace='sign')),
 
 	# Edit URLs
 	url(r'^fobi/', include('fobi.urls.edit')),
