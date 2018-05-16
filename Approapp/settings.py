27001fa0da44c0af1ef5b830f86638038ca72a35
Ripudaman Singh <rsingh_bemba16@thapar.edu>
2018-05-16 18:42:56 +0530

    postgres

--- Approapp/settings.py
+++ Approapp/settings.py
@@ -134,10 +134,9 @@ WSGI_APPLICATION = 'Approapp.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('postgres://qbhoialsiitwjk:a9c40c77e609002f7496b88a50425fd9fc2e12ce3a438519b08a26024b04afb4@ec2-23-21-121-220.compute-1.amazonaws.com:5432/dfc1vn6epml6nb')
+    )
 }
 
 
