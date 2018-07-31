d60e02ec9422678bca653008312e2c5ec14271ba
ammo <apabhishek178@gmail.com>
2018-07-31 22:16:58 +0530

    error

--- executive/views.py
+++ executive/views.py
@@ -50,12 +50,14 @@ __all__ = (
 def approve_form_entry(request, form_entry_id=None, feid=None):
     entries = SavedFormDataEntry.objects.get(form_entry__id=form_entry_id, id=feid)
     entries.approved = True
+    entries.approved_by = request.user.first_name
     entries.save()
     return redirect('savedformentry-detail', form_entry_id=form_entry_id, feid=feid)
 
 def disapprove_form_entry(request, form_entry_id=None, feid=None):
     entries = SavedFormDataEntry.objects.get(form_entry__id=form_entry_id, id=feid)
     entries.disapproved = True
+    entries.disapproved_by = request.user.first_name
     entries.save()
     return redirect('savedformentry-detail', form_entry_id=form_entry_id, feid=feid)
 
